3e66ecaa7171859d047c76c400907157faa977b1
Fernanda Winter <fernanda.winter@hotmail.com>
2023-04-13 23:33:34 -0300

    aula 2 e aula 3

---
+++ AULA 2 - TIPOS DE DADO E OPERADORES LÓGICOS/4 - strings/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof 'Oi, meu nome é Fernanda');
+console.log(typeof "Eu queria adotar um cachorro.");
+console.log(typeof `Teste`); 
+console.log(typeof "Infinity"); 
+console.log(typeof Infinity); 
